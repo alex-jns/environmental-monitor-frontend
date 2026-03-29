1cb38fc258c43f26ca3248976d45633dc9d9f6f4
alex-jns <159482010+alex-jns@users.noreply.github.com>
2026-03-29 18:27:22 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,103 @@
+/** Interfaces are an alternative way to provide types for components */
+interface MyButtonProps {
+  /** The text to display in the button */
+  title: string;
+  /** Whether the button can be interacted with */
+  disabled: boolean;
+}
+
+/** React apps are made of components */
+function MyButton({ title, disabled }: MyButtonProps) {
+  function handleClick() {
+    alert("You clicked me!");
+  }
+
+  return (
+    <button disabled={disabled} onClick={handleClick}>
+      {title}
+    </button>
+  );
+}
+
+function MyApp() {
+  return (
+    <body>
+      <div>
+        {/** Navbar content starts here */}
+        <nav className="navbar">
+          <div className="navbar-left">
+            {/** Left navbar is logo */}
+            <a href="/">Logo</a>
+          </div>
+
+          {/** Center navbar is links */}
+          <div className="navbar-center">
+            <ul className="nav-links">
+              <li>
+                <a href="/weather">Weather</a>
+              </li>
+
+              <li>
+                <a href="/report">Report</a>
+              </li>
+
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+
+          {/** Right navbar is icons */}
+          <div className="navbar-right">
+            <a href="/">Icon</a>
+          </div>
+        </nav>
+
+        {/** Main starts here */}
+        <main>
+          <div className="image-container">
+            <img
+              src="pexels-frostroomhead-18713933.jpg"
+              alt="A picture of clouds."
+            />
+            <h1 className="header-text">Environmental Monitor App</h1>
+            <p>Open from <time datetime="10:00">10:00 AM</time> to <time datetime="21:00">9:00 PM</time> every weekday.</p>
+
+
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+
+            <iframe
+              width="900"
+              height="300"
+              src="https://www.openstreetmap.org/export/embed.html?bbox=-82.21352577209474%2C36.58000536706752%2C-82.17327117919923%2C36.605916527931974&amp;layer=mapnik"
+              style={{ border: "1px solid black" }}
+            ></iframe>
+            <br />
+            <small>
+              <a target="_blank"
+              href="https://www.openstreetmap.org/?#map=15/36.59296/-82.19340">
+                View Larger Map
+              </a>
+            </small>
+          </div>
+        </main>
+
+        {/** Footer starts here */}
+        <footer>
+          <hr />
+          <div>Copyright 2026</div>
+        </footer>
+      </div>
+    </body>
+  );
+}
+
+export default MyApp;
