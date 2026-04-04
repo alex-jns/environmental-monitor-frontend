05cb202a7818290549557270720e015e81d13f76
alex-jns <159482010+alex-jns@users.noreply.github.com>
2026-04-04 13:56:45 -0400

    Sliced app into components

--- src/App.tsx
+++ src/App.tsx
@@ -4,252 +4,271 @@ import weatherData from "../../environmental-monitor-backend/bin/Debug/net10.0/r
 /** Formats temperatures to be no more than 2 decimals and no trailing zeroes. */
 const formatTemp = (temp: number) => parseFloat(temp.toFixed(2)).toString();
 
-/** Component for the inside weather dashboard */
+/** Top navigation bar. */
+const NavBar = () => {
+  return (
+    <div className="navbar bg-gradient-to-r from-slate-900 to-[#60298E]">
+      <NavBarLeft />
+      <NavBarCenter />
+      <NavBarRight />
+    </div>
+  );
+};
+
+/** Left navbar is logo. */
+const NavBarLeft = () => {
+  return (
+    <div className="navbar-left">
+      {/** Weather icons created by iconixar - Flaticon */}
+      <a href="/">
+        <img
+          src="cloudy.png"
+          alt="A picture of the sun partially obscured by a cloud."
+          className="w-8 h-8"
+        />
+      </a>
+    </div>
+  );
+};
+
+/** Center navbar is links. */
+const NavBarCenter = () => {
+  return (
+    <div className="navbar-center">
+      <ul className="nav-links">
+        <li>
+          <a href="/weather">Weather</a>
+        </li>
+
+        <li>
+          <a href="/report">Report</a>
+        </li>
+
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+/** Right navbar is icons. */
+const NavBarRight = () => {
+  return (
+    <div className="navbar-right">
+      <a href="/">Icon</a>
+    </div>
+  );
+};
+
+/** Shows a picture of clouds and the main site title. */
+const Banner = () => {
+  return (
+    <div className="image-container">
+      <img src="pexels-frostroomhead-18713933.jpg" alt="A picture of clouds." />
+      <h1 className="header-text pt-10">Environmental Monitor App</h1>
+    </div>
+  );
+};
+
+/** Specifies that the weather card component must take two strings. */
+interface WeatherCardProps {
+  title: string;
+  content: string;
+}
+
+/** Defines the weather card component which has two props: a title and content. */
+const WeatherCard = ({ title, content }: WeatherCardProps) => {
+  return (
+    <div className="weather-card bg-black/20 backdrop-blur-sm p-4 rounded-lg shadow">
+      <p className="text-sm text-white">{title}</p>
+      <h2 className="text-2xl font-bold">{content}</h2>
+    </div>
+  );
+};
+
+/** Component for the inside weather summary. */
+const InsideWeatherSummary = () => {
+  return (
+    <div className="bg-gradient-to-r from-slate-900 to-[#60298E] p-6">
+      <WeatherCard
+        title="Inside Summary"
+        content={weatherData.inside.inside_summary}
+      />
+    </div>
+  );
+};
+
+/** Component for the inside weather dashboard. */
 const InsideWeatherDashboard = () => {
   return (
     <div className="bg-gradient-to-r from-slate-900 to-[#60298E] p-6">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div className="weather-card bg-black/20 backdrop-blur-sm p-4 rounded-lg shadow">
-          <p className="text-sm text-white">Temperature</p>
-          <h2 className="text-2xl font-bold">
-            {formatTemp(weatherData.inside.temperatureF)} °F (
-            {formatTemp(weatherData.inside.temperatureC)} °C)
-          </h2>
-        </div>
+        <WeatherCard
+          title="Temperature"
+          content={`${formatTemp(weatherData.inside.temperatureF)} °F (
+            ${formatTemp(weatherData.inside.temperatureC)} °C)`}
+        />
 
-        <div className="weather-card bg-black/20 backdrop-blur-sm p-4 rounded-lg shadow">
-          <p className="text-sm text-white">Humidity</p>
-          <h2 className="text-2xl font-bold">
-            {formatTemp(weatherData.inside.humidity)}%
-          </h2>
-        </div>
+        <WeatherCard
+          title="Humidity"
+          content={`${formatTemp(weatherData.inside.humidity)}%`}
+        />
       </div>
     </div>
   );
 };
 
-/** Component for the inside weather summary */
-const InsideWeatherSummary = () => {
-  return (
-    <div className="bg-gradient-to-r from-slate-900 to-[#60298E] p-6">
-      <div className="weather-card bg-black/20 backdrop-blur-sm p-4 rounded-lg shadow">
-        <p className="text-sm text-white">Inside Summary</p>
-        <h2 className="text-2xl font-bold">
-          {weatherData.inside.inside_summary}
-        </h2>
-      </div>
-    </div>
-  );
-};
-
-/** Component for the outside weather dashboard */
+/** Component for the outside weather dashboard. */
 const OutsideWeatherDashboard = () => {
   return (
     <div className="bg-gradient-to-r from-slate-900 to-[#60298E] p-6">
+      {/** Header for the outside weather dashboard */}
       <h2 className="text-3xl font-bold underline">Outside</h2>
 
+      {/** Defines how the grid is structured for this component */}
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6">
-        <div className="weather-card bg-black/20 backdrop-blur-sm p-4 rounded-lg shadow">
-          <p className="text-sm text-white">Temperature</p>
-          <h2 className="text-2xl font-bold">
-            {formatTemp(weatherData.outside.temperature_2m_fahrenheit)} °F (
-            {formatTemp(weatherData.outside.temperature_2m)} °C)
-          </h2>
-        </div>
+        <WeatherCard
+          title="Temperature"
+          content={`${formatTemp(weatherData.outside.temperature_2m_fahrenheit)} °F
+          (${formatTemp(weatherData.outside.temperature_2m)} °C)`}
+        />
 
-        <div className="weather-card bg-black/20 backdrop-blur-sm p-4 rounded-lg shadow">
-          <p className="text-sm text-white">Humidity</p>
-          <h2 className="text-2xl font-bold">
-            {formatTemp(weatherData.outside.relative_humidity_2m)}%
-          </h2>
-        </div>
+        <WeatherCard
+          title="Humidity"
+          content={`${formatTemp(weatherData.outside.relative_humidity_2m)}%`}
+        />
 
-        <div className="weather-card bg-black/20 backdrop-blur-sm p-4 rounded-lg shadow">
-          <p className="text-sm text-white">Feels Like</p>
-          <h2 className="text-2xl font-bold">
-            {formatTemp(weatherData.outside.apparent_temperature_fahrenheit)} °F
-            ({formatTemp(weatherData.outside.apparent_temperature)} °C)
-          </h2>
-        </div>
+        <WeatherCard
+          title="Feels Like"
+          content={`${formatTemp(weatherData.outside.apparent_temperature_fahrenheit)} °F
+            (${formatTemp(weatherData.outside.apparent_temperature)} °C)`}
+        />
 
-        <div className="weather-card bg-black/20 backdrop-blur-sm p-4 rounded-lg shadow">
-          <p className="text-sm text-white">Daylight</p>
-          <h2 className="text-2xl font-bold">
-            {weatherData.outside.is_day_yesorno}
-          </h2>
-        </div>
+        <WeatherCard
+          title="Daylight"
+          content={weatherData.outside.is_day_yesorno}
+        />
 
-        <div className="weather-card bg-black/20 backdrop-blur-sm p-4 rounded-lg shadow">
-          <p className="text-sm text-white">Weather</p>
-          <h2 className="text-2xl font-bold">
-            {weatherData.outside.weather_name}
-          </h2>
-        </div>
+        <WeatherCard
+          title="Weather"
+          content={weatherData.outside.weather_name}
+        />
 
-        <div className="weather-card bg-black/20 backdrop-blur-sm p-4 rounded-lg shadow">
-          <p className="text-sm text-white">Cloudy</p>
-          <h2 className="text-2xl font-bold">
-            {weatherData.outside.cloud_cover}%
-          </h2>
-        </div>
+        <WeatherCard
+          title="Cloudy"
+          content={`${weatherData.outside.cloud_cover}%`}
+        />
 
-        <div className="weather-card bg-black/20 backdrop-blur-sm p-4 rounded-lg shadow">
-          <p className="text-sm text-white">Precipitation</p>
-          <h2 className="text-2xl font-bold">
-            {weatherData.outside.precipitation === 0 ? (
-              <p>None</p>
-            ) : (
-              <p>{weatherData.outside.precipitation} inches</p>
-            )}
-          </h2>
-        </div>
+        {/** Conditional in case there is no precipitation */}
+        <WeatherCard
+          title="Precipitation"
+          content={
+            // If precipitation is zero, say none, else show amount
+            weatherData.outside.precipitation === 0
+              ? "None"
+              : `{weatherData.outside.precipitation} inches`
+          }
+        />
 
-        <div className="weather-card bg-black/20 backdrop-blur-sm p-4 rounded-lg shadow">
-          <p className="text-sm text-white">Rain</p>
-          <h2 className="text-2xl font-bold">
-            {weatherData.outside.rain === 0 ? (
-              <p>None</p>
-            ) : (
-              <p>{weatherData.outside.rain} inches</p>
-            )}
-          </h2>
-        </div>
+        <WeatherCard
+          title="Rain"
+          content={
+            weatherData.outside.rain === 0
+              ? "None"
+              : `{weatherData.outside.rain} inches`
+          }
+        />
 
-        <div className="weather-card bg-black/20 backdrop-blur-sm p-4 rounded-lg shadow">
-          <p className="text-sm text-white">Showers</p>
-          <h2 className="text-2xl font-bold">
-            {weatherData.outside.showers === 0 ? (
-              <p>None</p>
-            ) : (
-              <p>{weatherData.outside.showers} inches</p>
-            )}
-          </h2>
-        </div>
+        <WeatherCard
+          title="Showers"
+          content={
+            weatherData.outside.showers === 0
+              ? "None"
+              : `{weatherData.outside.showers} inches`
+          }
+        />
 
-        <div className="weather-card bg-black/20 backdrop-blur-sm p-4 rounded-lg shadow">
-          <p className="text-sm text-white">Snowfall</p>
-          <h2 className="text-2xl font-bold">
-            {weatherData.outside.snowfall === 0 ? (
-              <p>None</p>
-            ) : (
-              <p>{weatherData.outside.snowfall} inches</p>
-            )}
-          </h2>
-        </div>
+        <WeatherCard
+          title="Snowfall"
+          content={
+            weatherData.outside.snowfall === 0
+              ? "None"
+              : `{weatherData.outside.snowfall} inches`
+          }
+        />
 
-        <div className="weather-card bg-black/20 backdrop-blur-sm p-4 rounded-lg shadow">
-          <p className="text-sm text-white">Wind Speed</p>
-          <h2 className="text-2xl font-bold">
-            {weatherData.outside.wind_speed_10m} miles per hour
-          </h2>
-        </div>
+        <WeatherCard
+          title="Wind Speed"
+          content={`${weatherData.outside.wind_speed_10m} miles per hour`}
+        />
 
-        <div className="weather-card bg-black/20 backdrop-blur-sm p-4 rounded-lg shadow">
-          <p className="text-sm text-white">Wind Direction</p>
-          <h2 className="text-2xl font-bold">
-            {weatherData.outside.wind_direction_10m_compass} (
-            {weatherData.outside.wind_direction_10m}°)
-          </h2>
-        </div>
+        {/** Shows where the wind is coming from, not where it's going */}
+        <WeatherCard
+          title="Wind Direction"
+          content={`${weatherData.outside.wind_direction_10m_compass}
+            (${weatherData.outside.wind_direction_10m}°)`}
+        />
       </div>
     </div>
   );
 };
 
-/** Main function of the app */
-function MyApp() {
+/** Taken from the OpenStreetMap site to embed on the site. */
+const OpenStreetMap = () => {
   return (
-    <body>
-      <div>
-        {/** Navbar content starts here */}
-        <nav className="navbar bg-gradient-to-r from-slate-900 to-[#60298E]">
-          <div className="navbar-left">
-            {/** Left navbar is logo */}
-            {/** Weather icons created by iconixar - Flaticon */}
-            <a href="/">
-              <img
-                src="cloudy.png"
-                alt="A picture of the sun partially obscured by a cloud."
-                className="w-8 h-8"
-              />
-            </a>
-          </div>
-
-          {/** Center navbar is links */}
-          <div className="navbar-center">
-            <ul className="nav-links">
-              <li>
-                <a href="/weather">Weather</a>
-              </li>
-
-              <li>
-                <a href="/report">Report</a>
-              </li>
-
-              <li>
-                <a href="/contact">Contact</a>
-              </li>
-            </ul>
-          </div>
-
-          {/** Right navbar is icons */}
-          <div className="navbar-right">
-            <a href="/">Icon</a>
-          </div>
-        </nav>
-
-        {/** Main starts here */}
-        <main>
-          <div className="image-container">
-            <img
-              src="pexels-frostroomhead-18713933.jpg"
-              alt="A picture of clouds."
-            />
-            <h1 className="header-text pt-10">Environmental Monitor App</h1>
-
-            <div>
-              <InsideWeatherSummary />
-            </div>
-
-            <div>
-              <InsideWeatherDashboard />
-              <OutsideWeatherDashboard />
-            </div>
-
-            <div className="w-200 mx-auto">
-              <iframe
-                width="900"
-                height="300"
-                src="https://www.openstreetmap.org/export/embed.html?bbox=-82.21352577209474%2C36.58000536706752%2C-82.17327117919923%2C36.605916527931974&amp;layer=mapnik"
-                style={{ border: "1px solid black" }}
-              ></iframe>
-              <br />
-              <small>
-                <a
-                  target="_blank"
-                  href="https://www.openstreetmap.org/?#map=15/36.59296/-82.19340"
-                >
-                  View Larger Map
-                </a>
-              </small>
-            </div>
-          </div>
-        </main>
-
-        {/** Footer starts here */}
-        <footer>
-          <hr />
-          <div>Copyright 2026</div>
-        </footer>
-      </div>
-    </body>
+    <div className="w-200 mx-auto">
+      <iframe
+        width="900"
+        height="300"
+        src="https://www.openstreetmap.org/export/embed.html?bbox=-82.21352577209474%2C36.58000536706752%2C-82.17327117919923%2C36.605916527931974&amp;layer=mapnik"
+        style={{ border: "1px solid black" }}
+      ></iframe>
+      <br />
+      <small>
+        <a
+          target="_blank"
+          href="https://www.openstreetmap.org/?#map=15/36.59296/-82.19340"
+        >
+          View Larger Map
+        </a>
+      </small>
+    </div>
   );
-}
+};
 
-function TestApp() {
+/** Footer element of the page. */
+const BottomBar = () => {
   return (
     <div>
-      <OutsideWeatherComponent />
+      <hr />
+      <div>Copyright 2026</div>
+    </div>
+  );
+};
+
+/** Main function of the app. */
+function MyApp() {
+  return (
+    <div>
+      {/** Header starts here */}
+      <header>
+        <NavBar />
+      </header>
+
+      {/** Main starts here */}
+      <main>
+        <Banner />
+        <InsideWeatherSummary />
+        <InsideWeatherDashboard />
+        <OutsideWeatherDashboard />
+        <OpenStreetMap />
+      </main>
+
+      {/** Footer starts here */}
+      <footer>
+        <BottomBar />
+      </footer>
     </div>
   );
 }
